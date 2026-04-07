a759386c5f2a5e3032f77b320f1e7ce66df7b1f5
Rafel <16695@uab.cat>
2026-04-07 17:30:18 +0200

    Simplify critter and collect-run behavior trees

--- AAgent_Python/BTCollectRun.py
+++ AAgent_Python/BTCollectRun.py
@@ -1,7 +1,6 @@
 import asyncio
 import random
 
-import py_trees
 import py_trees as pt
 from py_trees import common
 
@@ -11,99 +10,11 @@ import Goals_BT_Basic
 import Sensors
 
 
-CRITTER_TAG_FRAGMENTS = ("Critter",)
-
-
-def find_closest_visible_object(aagent, target_tags):
-    sensor_obj_info = aagent.rc_sensor.sensor_rays[Sensors.RayCastSensor.OBJECT_INFO]
-    sensor_angles = aagent.rc_sensor.sensor_rays[Sensors.RayCastSensor.ANGLE]
-    closest_target = None
-
-    for index, value in enumerate(sensor_obj_info):
-        if not value or not any(fragment in value["tag"] for fragment in target_tags):
-            continue
-
-        candidate = {
-            "index": index,
-            "distance": value["distance"],
-            "angle": sensor_angles[index],
-            "tag": value["tag"],
-        }
-        if closest_target is None or candidate["distance"] < closest_target["distance"]:
-            closest_target = candidate
-
-    return closest_target
-
-
-class CollectRunMemory:
-    def __init__(self):
-        self.was_frozen = False
-        self.pending_recovery = False
-
-
-class BN_DetectFrozenState(pt.behaviour.Behaviour):
-    def __init__(self, aagent, memory):
-        super(BN_DetectFrozenState, self).__init__("BN_DetectFrozenState")
-        self.my_agent = aagent
-        self.memory = memory
-
-    def initialise(self):
-        pass
-
-    def update(self):
-        if self.my_agent.i_state.isFrozen:
-            self.memory.was_frozen = True
-            return pt.common.Status.SUCCESS
-
-        if self.memory.was_frozen:
-            self.memory.was_frozen = False
-            self.memory.pending_recovery = True
-
-        return pt.common.Status.FAILURE
-
-    def terminate(self, new_status: common.Status):
-        pass
-
-
-class BN_WaitUntilThawed(pt.behaviour.Behaviour):
-    def __init__(self, aagent):
-        super(BN_WaitUntilThawed, self).__init__("BN_WaitUntilThawed")
-        self.my_agent = aagent
-
-    def initialise(self):
-        asyncio.create_task(self.my_agent.send_message("action", "stop"))
-
-    def update(self):
-        if self.my_agent.i_state.isFrozen:
-            return pt.common.Status.RUNNING
-        return pt.common.Status.SUCCESS
-
-    def terminate(self, new_status: common.Status):
-        pass
-
-
-class BN_ShouldRecoverAfterFreeze(pt.behaviour.Behaviour):
-    def __init__(self, memory):
-        super(BN_ShouldRecoverAfterFreeze, self).__init__("BN_ShouldRecoverAfterFreeze")
-        self.memory = memory
-
-    def initialise(self):
-        pass
-
-    def update(self):
-        if self.memory.pending_recovery:
-            self.memory.pending_recovery = False
-            return pt.common.Status.SUCCESS
-        return pt.common.Status.FAILURE
-
-    def terminate(self, new_status: common.Status):
-        pass
-
-
 class BN_DetectNearbyCritter(pt.behaviour.Behaviour):
     DANGER_DISTANCE = 6.0
 
     def __init__(self, aagent):
+        self.my_goal = None
         super(BN_DetectNearbyCritter, self).__init__("BN_DetectNearbyCritter")
         self.my_agent = aagent
 
@@ -111,9 +22,13 @@ class BN_DetectNearbyCritter(pt.behaviour.Behaviour):
         pass
 
     def update(self):
-        target = find_closest_visible_object(self.my_agent, CRITTER_TAG_FRAGMENTS)
-        if target and target["distance"] <= self.DANGER_DISTANCE:
-            return pt.common.Status.SUCCESS
+        sensor_obj_info = self.my_agent.rc_sensor.sensor_rays[Sensors.RayCastSensor.OBJECT_INFO]
+
+        for value in sensor_obj_info:
+            if value:
+                if "Critter" in value["tag"] and value["distance"] <= self.DANGER_DISTANCE:
+                    return pt.common.Status.SUCCESS
+
         return pt.common.Status.FAILURE
 
     def terminate(self, new_status: common.Status):
@@ -121,26 +36,16 @@ class BN_DetectNearbyCritter(pt.behaviour.Behaviour):
 
 
 class BN_EvadeCritter(pt.behaviour.Behaviour):
+    TOO_CLOSE_DISTANCE = 1.8
+
     def __init__(self, aagent):
+        self.my_goal = None
         super(BN_EvadeCritter, self).__init__("BN_EvadeCritter")
         self.my_agent = aagent
-        self.my_goal = None
 
-    def _choose_escape_turn(self, target):
-        if target is None:
-            return random.choice([-1, 1]) * random.uniform(120, 165)
-
-        angle = target["angle"]
-        if abs(angle) < 10:
-            return random.choice([-1, 1]) * random.uniform(135, 170)
-
-        if angle > 0:
-            return -random.uniform(100, 145)
-        return random.uniform(100, 145)
-
-    async def _run_escape(self, escape_turn, escape_distance):
-        target = find_closest_visible_object(self.my_agent, CRITTER_TAG_FRAGMENTS)
-        if target and target["distance"] <= 1.8:
+    async def evade(self, critter_distance, critter_angle):
+        # If the critter is almost touching the astronaut, back up first.
+        if critter_distance is not None and critter_distance <= self.TOO_CLOSE_DISTANCE:
             backed_off = await Goals_BT_Basic.BackwardDist(
                 self.my_agent,
                 random.uniform(0.8, 1.5),
@@ -150,22 +55,50 @@ class BN_EvadeCritter(pt.behaviour.Behaviour):
             if not backed_off:
                 return False
 
+        # Turn away from the critter. If it is directly ahead, choose a random side.
+        if critter_distance is None or abs(critter_angle) < 10:
+            escape_turn = random.choice([-1, 1]) * random.uniform(135, 170)
+        elif critter_angle > 0:
+            escape_turn = -random.uniform(100, 145)
+        else:
+            escape_turn = random.uniform(100, 145)
+
         turned = await Goals_BT_Basic.Turn_customizable(self.my_agent, 0, escape_turn).run()
         if not turned:
             return False
-        return await Goals_BT_Basic.ForwardDist(self.my_agent, escape_distance, 0, 0).run()
+
+        # After turning away, run forward to create space.
+        return await Goals_BT_Basic.ForwardDist(
+            self.my_agent,
+            random.uniform(4.5, 6.5),
+            0,
+            0,
+        ).run()
 
     def initialise(self):
-        target = find_closest_visible_object(self.my_agent, CRITTER_TAG_FRAGMENTS)
-        self.my_goal = asyncio.create_task(
-            self._run_escape(
-                self._choose_escape_turn(target),
-                random.uniform(4.5, 6.5),
-            )
-        )
+        sensor_obj_info = self.my_agent.rc_sensor.sensor_rays[Sensors.RayCastSensor.OBJECT_INFO]
+        sensor_angles = self.my_agent.rc_sensor.sensor_rays[Sensors.RayCastSensor.ANGLE]
+
+        critter_distance = None
+        critter_angle = 0.0
+
+        for index, value in enumerate(sensor_obj_info):
+            if value:
+                if "Critter" in value["tag"]:
+                    if critter_distance is None or value["distance"] < critter_distance:
+                        critter_distance = value["distance"]
+                        critter_angle = sensor_angles[index]
+
+        self.my_goal = asyncio.create_task(self.evade(critter_distance, critter_angle))
 
     def update(self):
-        return BTAlone.common_goal_update(self.my_goal)
+        if not self.my_goal.done():
+            return pt.common.Status.RUNNING
+        else:
+            if self.my_goal.result():
+                return pt.common.Status.SUCCESS
+            else:
+                return pt.common.Status.FAILURE
 
     def terminate(self, new_status: common.Status):
         if self.my_goal is not None:
@@ -174,19 +107,11 @@ class BN_EvadeCritter(pt.behaviour.Behaviour):
 
 class BN_ForcedRecoverAfterHit(pt.behaviour.Behaviour):
     def __init__(self, aagent):
+        self.my_goal = None
         super(BN_ForcedRecoverAfterHit, self).__init__("BN_ForcedRecoverAfterHit")
         self.my_agent = aagent
-        self.my_goal = None
 
-    def _choose_escape_turn(self):
-        target = find_closest_visible_object(self.my_agent, CRITTER_TAG_FRAGMENTS)
-        if target is None or abs(target["angle"]) < 12:
-            return random.choice([-1, 1]) * random.uniform(120, 165)
-        if target["angle"] > 0:
-            return -random.uniform(105, 150)
-        return random.uniform(105, 150)
-
-    async def _run_recovery(self):
+    async def recover(self, critter_angle):
         await self.my_agent.send_message("action", "stop")
         await asyncio.sleep(0)
 
@@ -197,11 +122,14 @@ class BN_ForcedRecoverAfterHit(pt.behaviour.Behaviour):
             0,
         ).run()
 
-        turned = await Goals_BT_Basic.Turn_customizable(
-            self.my_agent,
-            0,
-            self._choose_escape_turn(),
-        ).run()
+        if abs(critter_angle) < 12:
+            escape_turn = random.choice([-1, 1]) * random.uniform(120, 165)
+        elif critter_angle > 0:
+            escape_turn = -random.uniform(105, 150)
+        else:
+            escape_turn = random.uniform(105, 150)
+
+        turned = await Goals_BT_Basic.Turn_customizable(self.my_agent, 0, escape_turn).run()
         if not turned:
             return False
 
@@ -214,10 +142,29 @@ class BN_ForcedRecoverAfterHit(pt.behaviour.Behaviour):
         return bool(backed_off or escaped)
 
     def initialise(self):
-        self.my_goal = asyncio.create_task(self._run_recovery())
+        sensor_obj_info = self.my_agent.rc_sensor.sensor_rays[Sensors.RayCastSensor.OBJECT_INFO]
+        sensor_angles = self.my_agent.rc_sensor.sensor_rays[Sensors.RayCastSensor.ANGLE]
+
+        closest_distance = None
+        closest_angle = 0.0
+
+        for index, value in enumerate(sensor_obj_info):
+            if value:
+                if "Critter" in value["tag"]:
+                    if closest_distance is None or value["distance"] < closest_distance:
+                        closest_distance = value["distance"]
+                        closest_angle = sensor_angles[index]
+
+        self.my_goal = asyncio.create_task(self.recover(closest_angle))
 
     def update(self):
-        return BTAlone.common_goal_update(self.my_goal)
+        if not self.my_goal.done():
+            return pt.common.Status.RUNNING
+        else:
+            if self.my_goal.result():
+                return pt.common.Status.SUCCESS
+            else:
+                return pt.common.Status.FAILURE
 
     def terminate(self, new_status: common.Status):
         if self.my_goal is not None:
@@ -227,22 +174,25 @@ class BN_ForcedRecoverAfterHit(pt.behaviour.Behaviour):
 class BTCollectRun:
     def __init__(self, aagent):
         self.aagent = aagent
-        self.memory = CollectRunMemory()
+        self.recovery_memory = BTAlone.AloneRecoveryMemory()
 
         frozen = pt.composites.Sequence(name="DetectFrozen", memory=True)
         frozen.add_children([
-            BN_DetectFrozenState(aagent, self.memory),
-            BN_WaitUntilThawed(aagent),
+            BTAlone.BN_DetectFrozenState(aagent, self.recovery_memory),
+            BTAlone.BN_WaitUntilThawed(aagent),
         ])
 
         recover_from_hit = pt.composites.Sequence(name="RecoverFromHit", memory=True)
         recover_from_hit.add_children([
-            BN_ShouldRecoverAfterFreeze(self.memory),
+            BTAlone.BN_ShouldRecoverAfterFreeze(self.recovery_memory),
             BN_ForcedRecoverAfterHit(aagent),
         ])
 
         evade_critter = pt.composites.Sequence(name="EvadeCritter", memory=True)
-        evade_critter.add_children([BN_DetectNearbyCritter(aagent), BN_EvadeCritter(aagent)])
+        evade_critter.add_children([
+            BN_DetectNearbyCritter(aagent),
+            BN_EvadeCritter(aagent),
+        ])
 
         flower_protocol = pt.composites.Sequence(name="MoveToFlower", memory=True)
         flower_protocol.add_children([
